1db79f2941ae2dcd37c52b757e1246c404f20b59
Juan_Carlos_Glez_Pascolo <alu0100956269@ull.edu.es>
2017-10-19 17:01:20 +0100

    Inicializando las variables correctamente

--- Alimento/lib/Alimento/Comida.rb
+++ Alimento/lib/Alimento/Comida.rb
@@ -1,9 +1,12 @@
 
 class Comida
-  attr_reader :name
+  attr_reader :name, :proteins, :carbohydrates, :lipids
   
-  def initialize (name_value)
+  def initialize (name_value, proteins_value, carbohydrates_value, lipids_value)
     @name = name_value
+    @proteins = proteins_value
+    @carbohydrates = carbohydrates_value
+    @lipids = lipids_value
   end
 
 end
--- Alimento/spec/Alimento_spec.rb
+++ Alimento/spec/Alimento_spec.rb
@@ -6,15 +6,29 @@ require 'Alimento/Comida.rb'
 
 describe Alimento do
   before :all do
-    @huevo = Comida.new("Huevo")
+    @huevo = Comida.new("Huevo", 14.1, 0.0, 19.5)
   end
 
   it 'has a version number' do
     expect(Alimento::VERSION).not_to be nil
   end
 
-  it 'has a name' do
-    expect(@huevo.name).to eq("Huevo")
+  describe "# Inicializaciones correctas" do
+    it 'has a name' do
+      expect(@huevo.name).to eq("Huevo")
+    end
+
+    it 'has a number of proteins' do
+      expect(@huevo.proteins).to eq(14.1)
+    end
+
+    it 'has a number of carbohydrates' do
+      expect(@huevo.carbohydrates).to eq(0.0)
+    end
+
+    it 'has a number of lipids' do
+      expect(@huevo.lipids).to eq(19.5)
+    end
   end
 
   it 'does something useful' do
