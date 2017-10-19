1f2f81c0a258b6aa88ab718a75646ad4d0197b8d
Juan_Carlos_Glez_Pascolo <alu0100956269@ull.edu.es>
2017-10-19 16:32:25 +0100

    Primera prueba, que se cree la clase y que tenga nombre

---
+++ Alimento/lib/Alimento/Comida.rb
@@ -0,0 +1,9 @@
+
+class Comida
+  attr_reader :name
+  
+  def initialize (name_value)
+    @name = name_value
+  end
+
+end
--- Alimento/spec/Alimento_spec.rb
+++ Alimento/spec/Alimento_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
+require 'minitest/autorun'
+require 'minitest/spec'
+$:.unshift File.expand_path(File.dirname(__FILE__) + 'lib')
+require 'Alimento/Comida.rb'
 
 describe Alimento do
+  before :all do
+    @huevo = Comida.new("Huevo")
+  end
+
   it 'has a version number' do
     expect(Alimento::VERSION).not_to be nil
+  end
+
+  it 'has a name' do
+    expect(@huevo.name).to eq("Huevo")
   end
 
   it 'does something useful' do
