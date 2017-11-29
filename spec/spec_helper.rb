3e2bf0f2686f905a6649403513be545405d4cd45
Juan_Carlos_Glez_Pascolo <alu0100956269@ull.edu.es>
2017-11-29 10:07:40 +0000

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'Alimento'
 require 'coveralls'
-Coverall.wear!
+Coveralls.wear!
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'Alimento' 
+
